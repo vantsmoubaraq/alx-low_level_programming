ba681025de929f9290bbf1f53976f543f16dbba0
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-06-27 22:03:27 -0700

    compile

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,3 +1,5 @@
-int main(){
-return 0;
+#include <stdio.h>
+
+void main(){
+printf("Hello, World");
 }
--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,5 +1,9 @@
 	.file	"main.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"Hello, World"
+	.text
 	.globl	main
 	.type	main, @function
 main:
@@ -11,7 +15,10 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rdi
 	movl	$0, %eax
+	call	printf@PLT
+	nop
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
