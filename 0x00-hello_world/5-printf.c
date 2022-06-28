18012ace96b17a44835a9316e80bf9ead97857da
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-06-27 22:54:46 -0700

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Entry is main
+ *
+ * Return 0 (Success)
+ */
+
+int main(void)
+{
+   printf("with proper grammar, but the outcome is a piece of art,\n");
+   return 0;
+
+
+}
+
