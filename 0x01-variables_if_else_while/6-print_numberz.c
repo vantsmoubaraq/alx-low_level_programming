d36de83530630094508f9805d3fec59a075454b6
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-01 04:22:47 -0700

    letters

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry Point
+ *
+ * Description - Print all numbers 0 to 9
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int num;
+
+for(num = 0; num < 10; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
