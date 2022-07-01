5f08493eed271858a32173bbcb40134909b85c48
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-01 00:55:31 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description - print numbers 0 to 9
+ *
+ *Return: always return 0
+ */
+
+int main(void)
+{
+int num;
+
+for (num = 0; num < 10; num++)
+	printf("%d", num);
+	putchar('\n');
+return (0);
+}
