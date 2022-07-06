4ed70f7d59c3b0d40646a1097ab04b03a17b0dbf
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-06 13:43:14 -0700

    debug

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -23,15 +23,5 @@ int main(void)
 
 	print_remaining_days(month, day, year);
 
-	month = 3;
-	day = 1;
-	year = 2000;
-
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-	day = convert_day(month, day);
-
-	print_remaining_days(month, day, year);
-
 	return (0);
 }
