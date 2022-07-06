ef72b90e26419f22aa76fdd9df0a305bae3c11a8
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-05 17:23:36 -0700

    alpha

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,7 +36,8 @@ void print_times_table(int n)
 				else if (res >= 100 && j != 0)
 				{
 					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');												_putchar((res % 10) + '0');
+					_putchar((res / 10 % 10) + '0');
+					_putchar((res % 10) + '0');
 				}
 				else
 				{
