e6ac5284370df38c992befeedeeb37d743e89ed8
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-06 11:12:03 -0700

    debug

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,15 +6,11 @@
 
 #include "main.h"
 
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-
 /**
  *positive_or_negative - produces negative or positive number
  *No Description:
  *@n: number to be tested
- *Return: 0
+ *Return: Nothing
  */
 
 void positive_or_negative(int n)
