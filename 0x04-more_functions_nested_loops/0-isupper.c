8be5dbee60b7ed89b77b26db18a317f2c9b22a05
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-07 01:17:40 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase character
+ *
+ *@c: int
+ *
+ *Return: 1 if upper, 0 other
+ */
+
+int _isupper(int c)
+{
+	char alpha;
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		if (c == alpha)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
