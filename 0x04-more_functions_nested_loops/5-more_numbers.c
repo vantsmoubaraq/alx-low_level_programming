8f297fce1d3ca358e92136c54fbbed8ba8f210a1
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-07 05:49:28 -0700

    functions

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,14 +14,14 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num >= 10)
+			if (num < 10)
 			{
-				_putchar(num / 10 + '0');
-				_putchar(num % 10 + '0');
+				_putchar (num + '0');
 			}
 			else
 			{
-			_putchar (num + '0');
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
 			}
 		}
 		_putchar('\n');
