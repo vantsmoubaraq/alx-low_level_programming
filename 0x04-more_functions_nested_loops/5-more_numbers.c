3a161ce5e82681d65a42ce06cbe96b2f580f9117
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-07 05:34:59 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1-14 10x
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int cycle, num;
+
+	for (cycle = 1; cycle <= 10; cycle++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+			{
+				_putchar(num / 10 + '0');
+				_putchar(num % 10 + '0');
+			}
+			else
+			{
+			_putchar (num + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
