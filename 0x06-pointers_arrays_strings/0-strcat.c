f4412c26c82b6fdec15d7d58155edbea7cc7ada0
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-07-13 08:07:47 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,12 +14,12 @@ char *_strcat(char *dest, char *src)
 	int b;
 
 	a = 0;
-	while (dest[a] != 0)
+	while (dest[a])
 	{
 		a++;
 	}
 	b = 0;
-	while (src[b] != 0)
+	while (src[b])
 	{
 		dest[a] = src[b];
 		a++;
