0c5483359868a78c30cf7057d94a47f7bd7821f6
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-09-29 05:24:05 -0700

    Doubly_linked_lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -15,17 +15,17 @@ size_t dlistint_len(const dlistint_t *h)
 	const dlistint_t *current;
 	size_t count = 0;
 
-	if(h == NULL)
+	if (h == NULL)
 	{
 		return (0);
 	}
 
 	current = h;
 
-	while(current)
+	while (current)
 	{
 		count++;
 		current = current->next;
 	}
-	return count;
+	return (count);
 }
