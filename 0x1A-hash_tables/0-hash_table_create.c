22863c3f2cc62dbae551375e53394164c7d87757
vantsmoubaraq <vantsmoubaraq@gmail.com>
2022-10-20 21:02:04 -0700

    Hash Tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new->size = size;
-	new->array = malloc(size * sizeof(hash_node_t));
+	new->array = malloc(size * sizeof(hash_node_t *));
 
 	if ((new->array) == NULL)
 	{
