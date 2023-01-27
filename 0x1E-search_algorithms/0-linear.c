7192c3c4b8e620b40466f53dd1a354aab280801f
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-01-27 08:40:51 +0300

    search_algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,18 +11,16 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	unsigned int i;
 
 	if (array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		{
+		printf("value checked array[%d] = [%d]\n", i, *(array + i));
+		if (*(array + i) == value)
 			return (i);
-		}
 	}
 	return (-1);
 }
